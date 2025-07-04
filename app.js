fa31d8d01d8585b2220f5299945a740acb926b76
Likeshkumarsahu <kumarsahulikesh88@gmail.com>
2025-07-04 20:09:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+let userScore = 0;
+let compScore = 0;
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+};
+
+const drawGame = () => {
+    msg.innerText = "It's a draw!";
+    msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+    if (userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You win! ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    } else {
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = `You lost! ${compChoice} beats ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+};
+
+const playGame = (userChoice) => {
+    const compChoice = genCompChoice();
+
+    if (userChoice === compChoice) {
+        drawGame();
+    } else {
+        let userWin = true;
+        if (
+            (userChoice === "rock" && compChoice === "paper") ||
+            (userChoice === "paper" && compChoice === "scissors") ||
+            (userChoice === "scissors" && compChoice === "rock")
+        ) {
+            userWin = false;
+        }
+        showWinner(userWin, userChoice, compChoice);
+    }
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
